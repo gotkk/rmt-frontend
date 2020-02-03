38210b4b83b8d16e34bcfbf96f711c1b3b0312a3
KDev <sk555av@gmail.com>
2020-02-03 17:55:18 +0700

    add vuetify, header nav

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <Header />
+    <v-content>
+      <!-- <div id="app">
+        <div id="nav">
+          <router-link to="/">Home</router-link>|
+          <router-link to="/about">About</router-link>
+        </div>
+      </div>-->
+      <router-view />
+      <!-- <div style="backgroundColor: red; height: 50px">sss</div> -->
+    </v-content>
+    <Footer />
+  </v-app>
 </template>
 
+
+<script>
+import Header from "@/components/layouts/Header.vue";
+import Footer from "@/components/layouts/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list dense>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-home-circle</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>หน้าหลัก</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item  to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-clock-alert</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>แจ้งเตือน</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item  to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-clipboard-text</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>สัญญาเช่า</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item  to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-google-maps</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>ที่ดิน</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item  to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-flash</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>ไฟฟ้า</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item  to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-water-pump</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>น้ำประปา</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+
+// https://cdn.materialdesignicons.com/2.0.46/
+
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: false
+  }),
+  updated() {
+    console.log(this.drawer);
+  },
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
+};
+</script>
+
+<style>
+</style>
